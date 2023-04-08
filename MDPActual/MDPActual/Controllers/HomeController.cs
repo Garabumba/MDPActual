46c5fae3553c7a8d8d7a1e9eb17bf2cbe456bec5
Garabumba <kostya1473@gmail.com>
2023-04-08 21:27:26 +0300

    added the ability to read item names

---
+++ MDPActual/MDPActual/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using MDPActual.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ExcelDataReader;
+using System.Data;
+using System.IO;
+
+namespace MDPActual.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<string> names = new List<string>();
+        public static string fileName = "";
+        private string[] path;
+        public static int[] position;
+
+        [HttpGet]
+        public IActionResult Index(List<string> items = null)
+        {
+
+            names = items == null ? new List<string>() : items;
+            return View(names);
+        }
+        public IActionResult Index(IFormFile file, [FromServices] Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment)
+        {
+            //Для теста
+            fileName = $"{hostingEnvironment.WebRootPath}\\files\\{file.FileName}";
+            path = Directory.GetCurrentDirectory().Split("\\");
+            //Для билда
+            /*for (int i = 0; i < path.Length - 3; i++)
+                fileName += $"{path[i]}\\";
+            fileName += $"{@"\wwwroot\files"}" + "\\" + file.FileName;
+            Console.WriteLine(fileName);*/
+
+            using (FileStream fileStream = System.IO.File.Create(fileName))
+            {
+                file.CopyTo(fileStream);
+                fileStream.Flush();
+            }
+            GetItemList();
+            return Index(names);
+        }
+        private List<string> GetItemList()
+        {
+            ExcelReader er = new ExcelReader(3, fileName);
+            er.SearchNameOfItems(names);
+            return names;
+            //System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            //using (var stream = System.IO.File.Open(fileName, FileMode.Open, FileAccess.Read))
+            //{
+            //    using (var reader = ExcelReaderFactory.CreateOpenXmlReader(stream))
+            //    {
+            //        DataTable specificWorkSheet = reader.AsDataSet().Tables[3];
+            //        SearchRow sr = new SearchRow();
+            //        position = sr.Search(specificWorkSheet, "наименование");
+            //        int startRow = position[0];
+            //        int startColumn = position[1];
+            //        int currentRow = 0;
+
+            //        foreach (var row in specificWorkSheet.Rows)
+            //        {
+            //            if (currentRow > startRow && ((DataRow)row)[startColumn].ToString() != "")
+            //                names.Add(((DataRow)row)[startColumn].ToString());
+            //            currentRow++;
+            //        }
+            //    }
+            //}
+            //return names;
+        }
+    }
+}
---
+++ MDPActual/MDPActual/Models/ExcelReader.cs
@@ -0,0 +1,62 @@
+﻿using ExcelDataReader;
+using System.Data;
+
+namespace MDPActual.Models
+{
+    //Класс для читания эксель файла
+    public class ExcelReader
+    {
+        private int pageNumber;
+        private string fileName;
+        //конструктор для чтения нужной таблицы
+        public ExcelReader(int pageNumber, string fileName)
+        {
+            this.pageNumber = pageNumber;
+            this.fileName = fileName;
+        }
+
+        public List<string> SearchNameOfItems(List<string> array = null)
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var stream = System.IO.File.Open(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateOpenXmlReader(stream))
+                {
+                    DataTable specificWorkSheet = reader.AsDataSet().Tables[pageNumber];
+                    int[] position = new int[2];
+                    position = SearchPosition(specificWorkSheet, "наименование");
+                    int startRow = position[0];
+                    int startColumn = position[1];
+                    int currentRow = 0;
+
+                    foreach (var row in specificWorkSheet.Rows)
+                    {
+                        if (currentRow > startRow && ((DataRow)row)[startColumn].ToString() != "")
+                            array.Add(((DataRow)row)[startColumn].ToString());
+                        currentRow++;
+                    }
+                }
+            }
+            return array;
+        }
+
+        public int[] SearchPosition(DataTable dt, string name)
+        {
+            int currentRow = 0;
+            int[] position = new int[2];
+
+            foreach (var row in dt.Rows)
+            {
+                for (int currentColumn = 0; currentColumn <= 5; currentColumn++)
+                    if (((DataRow)row)[currentColumn].ToString().ToLower() == name.ToLower())
+                    {
+                        position[0] = currentRow;
+                        position[1] = currentColumn;
+                        break;
+                    }
+                currentRow++;
+            }
+            return position;
+        }
+    }
+}
